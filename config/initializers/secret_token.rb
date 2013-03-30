132c86dc2b38e93d81658beb6093f22fb9600a0d
Nicholas Sweeney <nsweeney333@gmail.com>
2013-03-30 16:49:23 -0400

    2nd version from new directory.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FamilywebTreehouseTreebookApp::Application.config.secret_token = 'e887e2cd641bcd46dc62503bf2ffad0f2d18b9a174a4fc63b8887ba392260379275a1bb280d02d6bb6d79d2f5935aee415122ed2119312577241ea92485bc05a'
