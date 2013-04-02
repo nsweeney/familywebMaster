b76b5830cf5447ded654e63e68ed4168f5b7eda4
Nick Sweeney <nsweeney333@gmail.com>
2013-04-02 12:51:12 -0400

    Updated frontend for the new authentication system.
    Added simple_form
    Updated several forms to bootstrap/simple_form
    Added login/logout/register/profile links to our nav
    Added a collection select to our status form
    :wq

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,5 @@
 class Status < ActiveRecord::Base
-  attr_accessible :content, :name
+  attr_accessible :content, :user_id
+  # below line we added to set up db relationship
+  belongs_to :user   
 end
