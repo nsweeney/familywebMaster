0ffe9485b88ce59114f3e4c305d124e571707a41
Nick Sweeney <nsweeney333@gmail.com>
2013-04-03 06:44:04 -0400

    Added by myself, validattion for status.rb page

--- app/models/status.rb
+++ app/models/status.rb
@@ -2,4 +2,7 @@ class Status < ActiveRecord::Base
   attr_accessible :content, :user_id
   # below line we added to set up db relationship
   belongs_to :user   
+  
+  validates :user, presence: true
+  validates :content, presence: true
 end
--- test/unit/status_test.rb
+++ test/unit/status_test.rb
@@ -4,4 +4,15 @@ class StatusTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  # test 1 for making sure user name selected
+  #test "a status should have a user name associated with it" do
+   # user = User.new
+   # assert !user.save
+   # assert !user.errors[:user].empty?
+ # end     ///the test preceding doesnt work, but the validates I was trying to accomplish, making sure a user name is selected does work from status.rb
+  
+  # test 2 for making sure some content is entered
+  # not sure how to write the test for content either
+  
 end
