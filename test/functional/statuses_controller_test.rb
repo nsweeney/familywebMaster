a69273e98323dc2b95341e2fefe2fe8188809d45
Nick Sweeney <nsweeney333@gmail.com>
2013-04-05 09:00:14 -0400

    scoped status updates to current user on create and update

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -1,3 +1,4 @@
+#old v
 require 'test_helper'
 
 class StatusesControllerTest < ActionController::TestCase
@@ -38,6 +39,17 @@ class StatusesControllerTest < ActionController::TestCase
 
     assert_redirected_to status_path(assigns(:status))
   end
+  
+  test "should create status for the current user when logged in" do
+    sign_in users(:jason)
+
+    assert_difference('Status.count') do
+      post :create, status: { content: @status.content, user_id: users(:jim).id }  
+    end
+
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:jason).id
+  end
 
   test "should show status" do
     get :show, id: @status
@@ -67,10 +79,24 @@ class StatusesControllerTest < ActionController::TestCase
     put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
+  
+  test "should update status for the current user when logged in" do
+    sign_in users(:jason)
+    put :update, id: @status, status: { content: @status.content, user_id: users(:jim).id }
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:jason).id
+  end
+  
+  test "should not update the status if nothing has changed" do
+    sign_in users(:jason)
+    put :update, id: @status
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:jason).id
+  end
 
-  test "should destroy status" do
-    assert_difference('Status.count', -1) do
-      delete :destroy, id: @status
+   test "should destroy status" do
+    assert_difference('Status.count', -1) do   # Issue here!
+      delete :destroy, id: @status             # Issue here!
     end
 
     assert_redirected_to statuses_path
